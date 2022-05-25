90e0c51937c3eb2684ce159f16f9a122e32fe73d
Kiril Nedyalkov <k.nedialkov@gmail.com>
2022-05-25 18:24:37 +0300

    update of config with class Config

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # shortener_for_url
  test project for url shortener
+
+https://realpython.com/build-a-python-url-shortener-with-fastapi/#demo-your-python-url-shortener
+
--- shortener_app/config.py
+++ shortener_app/config.py
@@ -3,17 +3,21 @@ from pydantic import BaseSettings
 
 from functools import lru_cache
 
-"""The BaseSettings class comes in handy to define environment variables in your application. 
-You only have to define the variables that you want to use, and pydantic takes care of the rest. 
-In other words, pydantic will automatically assume those default values if it doesn't find the corresponding 
-environment variables."""
+
 class Settings(BaseSettings):
+    """The BaseSettings class comes in handy to define environment variables in your application.
+    You only have to define the variables that you want to use, and pydantic takes care of the rest.
+    In other words, pydantic will automatically assume those default values
+    if it doesn't find the corresponding environment variables."""
     env_name: str = "Local"                     # Name of your current environment
     base_url: str = "http://localhost:8000"     # Domain of your app
     db_url: str = "sqlite:///./shortener.db"    # Address of your database
 
+    class Config:
+        env_file = ".env"
 
-@lru_cache
+
+@lru_cache                  # lru decorator to cache settings of the app
 def get_settings():
     settings = Settings()
     print(f"Loading settings for: {settings.env_name}")
