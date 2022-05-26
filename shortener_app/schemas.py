a8bfc3eca9717a97028980a8b507d2e036eead7e
Kiril Nedyalkov <k.nedialkov@gmail.com>
2022-05-26 13:29:45 +0300

    Some info about classes in schemas

--- shortener_app/schemas.py
+++ shortener_app/schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 
 class URLBase(BaseModel):
+    """target_url to store the URL that your shortened URL forwards to."""
     target_url: str
 
 
@@ -10,9 +11,16 @@ class URL(URLBase):
     clicks: int
 
     class Config:
+        """you tell pydantic with orm_mode = True to work with a database model.
+        ORM stands for Object-Relational Mapping, and it provides the convenience
+        of interacting with your database using an object-oriented approach."""
         orm_mode = True
 
 
 class URLInfo(URL):
+    """ This enhances URL by requiring two additional strings, url and admin_url.
+    You could also add the two strings url and admin_url to URL.
+    But by adding url and admin_url to the URLInfo subclass,
+    you can use the data in your API without storing it in your database."""
     url: str
     admin_url: str
