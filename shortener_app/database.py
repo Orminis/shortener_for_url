6dab2625b65e7dfee7ab933b620ecb9d8d0a7412
Kiril Nedyalkov <k.nedialkov@gmail.com>
2022-05-26 15:59:17 +0300

    database connection code in database.py

---
+++ shortener_app/database.py
@@ -0,0 +1,24 @@
+# database.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from .config import get_settings
+
+# You can think of engine as the entry point to your database.
+engine = create_engine(
+    get_settings().db_url, connect_args={"check_same_thread": False}
+    # With this connection argument, SQLite allows more than one request at a time to communicate with the database.
+)
+
+# You use sessionmaker, which you import in line 5, to create the SessionLocal class in line 12.
+# You’ll create a working database session when you instantiate SessionLocal later.
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
+
+"""The declarative_base function that you import in line 4 returns a class that connects the database engine
+ to the SQLAlchemy functionality of the models. You assign declarative_base() to Base in line 15. 
+ Base will be the class that the database model inherits from in your models.py file."""
+Base = declarative_base()
