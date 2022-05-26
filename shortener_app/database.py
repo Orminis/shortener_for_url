9e7dd54e160c8025a0aa23bab47fb619dddd5d70
Kiril Nedyalkov <k.nedialkov@gmail.com>
2022-05-26 16:00:48 +0300

    small update of database.py

--- shortener_app/database.py
+++ shortener_app/database.py
@@ -12,13 +12,13 @@ engine = create_engine(
     # With this connection argument, SQLite allows more than one request at a time to communicate with the database.
 )
 
-# You use sessionmaker, which you import in line 5, to create the SessionLocal class in line 12.
-# You’ll create a working database session when you instantiate SessionLocal later.
+"""You use sessionmaker, which you import in line 5, to create the SessionLocal class in line 12.
+   You’ll create a working database session when you instantiate SessionLocal later."""
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine
 )
 
 """The declarative_base function that you import in line 4 returns a class that connects the database engine
- to the SQLAlchemy functionality of the models. You assign declarative_base() to Base in line 15. 
- Base will be the class that the database model inherits from in your models.py file."""
+to the SQLAlchemy functionality of the models.
+Base will be the class that the database model inherits from in your models.py file."""
 Base = declarative_base()
